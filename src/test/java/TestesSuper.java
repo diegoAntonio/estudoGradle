a343437b8a71f53a35c06914a6859fadf127c37f
Diego Ferreira <diego.ferreira.nti@ufpe.br>
2016-05-16 09:44:18 -0300

    notacao deployment esquecida nos testes

--- src/test/java/TestesSuper.java
+++ src/test/java/TestesSuper.java
@@ -1,14 +1,14 @@
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
-@Ignore
 public abstract class TestesSuper {
 	
+	@Deployment
 	public static Archive<?> vaiBusanfa() {
 		return ShrinkWrap.create(JavaArchive.class, "teste.jar").addClass(ClasseModelo.class);
 	}
